9d6d9cff691b286bff9bba767ffeeda8a9e532c1
yvan-dz <yvandzefak1@gmail.com>
2025-01-06 20:11:06 +0100

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory, session
 from flask_mail import Mail, Message
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from werkzeug.utils import secure_filename
+from urllib.parse import quote as url_quote
 from supabase import create_client, Client
 import os
 
